058d2c3c982ba708510c8fb8fcdfd4be6044de36
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-15 20:14:05 +0100

    Create 'base.html' and 'sidebar.html' templates

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = "to_do_list.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
