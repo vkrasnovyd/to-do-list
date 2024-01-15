c214a59201560482706f555b197e661c46a7dd20
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-15 22:48:13 +0100

    Change ordering for Tag model

--- TODO_list/models.py
+++ TODO_list/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=63)
 
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self):
         return self.name
 
