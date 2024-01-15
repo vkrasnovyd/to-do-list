58bba01332934ff84f5bd91cbb7398ead39736a1
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-15 19:53:39 +0100

    Install django debug toolbar

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "TODO_list",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
