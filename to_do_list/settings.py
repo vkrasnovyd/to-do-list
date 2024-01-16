dc18af64e86ec63e918edec94b89aa2d0c9ec3c7
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-16 13:55:33 +0100

    Move SECRET_KEY variable to '.env' file

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from dotenv import load_dotenv
+import os
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ew-ca2gk)cl23wf1ufz)xete$r^=qp7)*p8g^o#urpk7wye=x0"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
